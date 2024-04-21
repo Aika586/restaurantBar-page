00e9a080f57a0828f46025895145adf9ee09fbd6
Aikokul <baatirbekovna.6@mail.ru>
2024-04-21 23:39:29 +0600

    last commit

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const textContainer1 = document.getElementsByClassName("text-container1")[0];
 const textContainer2 = document.getElementsByClassName("text-container2")[0];
-const sliderContainer=document.getElementsByClassName("swiper-wrapper")[0];
+const sliderContainer = document.getElementsByClassName("swiper-wrapper")[0];
+const uls = document.querySelectorAll(".phone-list,.email-list");
+const socialMediaContainer=document.getElementsByClassName('socialMedia-list')[0]
+
+
+
+
+
 
 //CREATE A TEXT CONTAINER
 const createTextList = (element) => {
@@ -20,7 +27,7 @@ const createTextList = (element) => {
     element.appendChild(li);
   });
 };
-createTextList( textContainer1);
+createTextList(textContainer1);
 createTextList(textContainer2);
 //create gallery container
 const imgSrc = [
@@ -32,42 +39,105 @@ const imgSrc = [
   "./assets/gallery/2.png",
   "./assets/gallery/3.png",
   "./assets/gallery/4.png",
-
 ];
 
-  imgSrc.forEach((src, i) => {
-    const img = document.createElement("img");
-    const sliderItem=document.createElement("div")
-    sliderItem.classList.add("swiper-slide")
-    img.src = src;
-    img.classList.add('images')
-    img.alt = "Image " + (i + 1);
-    sliderItem.appendChild(img);
-    sliderContainer.appendChild(sliderItem)
+imgSrc.forEach((src, i) => {
+  const img = document.createElement("img");
+  const sliderItem = document.createElement("div");
+  sliderItem.classList.add("swiper-slide");
+  img.src = src;
+  img.classList.add("images");
+  img.alt = "Image " + (i + 1);
+  sliderItem.appendChild(img);
+  sliderContainer.appendChild(sliderItem);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper(".swiper-container", {
+    slidesPerView: 4,
+    slidesPerGroup: 4, // Adjust the number of slides per view as needed
+    spaceBetween: 50,
+    direction: "horizontal",
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next", // If you need navigation buttons
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+      delay: 3000, // Adjust the autoplay delay (in milliseconds) as needed
+      disableOnInteraction: false, // Allow autoplay to continue after user interaction
+    },
+  });
+});
+
+// FORM _SECTION !!!!!!
+
+document
+  .getElementById("reservation-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById("custumer_name").value;
+    const phone = document.getElementById("phone_number").value;
+
+    const phonePattern = /^\+996\s\d{3}\s\d{2}\s\d{2}\s\d{2}$/;
+    const namePattern = /^[a-zA-Zа-яА-Я\s]+$/;
+    if (!phonePattern.test(phone)) {
+      alert("Пожалуйста, введите корректный формат телефона.");
+      return;
+    }
+
+    if (!namePattern.test(name)) {
+      alert("Пожалуйста, введите корректное имя.");
+      return;
+    }
+
+    alert("Вы успешно отправили заявку!!!");
   });
 
-  document.addEventListener('DOMContentLoaded', function () {
-    const swiper = new Swiper('.swiper-container', {
-      slidesPerView: 4,
-      slidesPerGroup: 4, // Adjust the number of slides per view as needed
-      spaceBetween: 50, 
-      direction: 'horizontal',
-      loop:true,
-      navigation: {
-        nextEl: '.swiper-button-next', // If you need navigation buttons
-        prevEl: '.swiper-button-prev',
-      },
-      autoplay: {
-        delay: 3000, // Adjust the autoplay delay (in milliseconds) as needed
-        disableOnInteraction: false, // Allow autoplay to continue after user interaction
-      },
-    });
-    console.log(swiper)
+//FOOTER SECTION!!!!!
+
+const phoneList = [
+  "/ Позвоните нам",
+  "+996 501 06 55 05",
+  "+996 501 06 55 05",
+  "+996 501 06 55 05",
+];
+const emailList = [
+  "/ Пищите нам",
+  "боно2gmail.com",
+  "боно2gmail.com",
+  "боно2gmail.com",
+];
+const socialMedias = [
+  { iconSrc: "./assets/icons/Group.png", link: "https://www.instagram.com" },
+  { iconSrc: "./assets/icons/Telegram.png", link: "https://www.instagram.com" },
+  { iconSrc: "./assets/icons/Facebook.png", link: "https://www.facebook.com" },
+  { iconSrc: "./assets/icons/TikTok.png", link: "https://www.TikTok.com" },
+  { iconSrc: "./assets/icons/SVG.png", link: "https://www.youtube.com" }
+];
+
+const ulListCreator = (textList, element) => {
+  textList.forEach((text) => {
+    const li = document.createElement("li");
+    li.textContent = text;
+    element.appendChild(li);
   });
+};
+ulListCreator(phoneList, uls[0]);
+ulListCreator(emailList, uls[1]);
 
-  // FORM _SECTION !!!!!!
-
-
-
-
-
+socialMedias.forEach(media=>{
+  const li=document.createElement("li")
+  const a=document.createElement("a")
+  const img=document.createElement("img")
+  a.appendChild(img);
+  li.appendChild(a);
+  a.href=media.link;
+  a.target="_blank";
+  a.rel='noopener noreferrer';
+  img.src=media.iconSrc;
+  
+  socialMediaContainer.append(li)
+ 
+})
